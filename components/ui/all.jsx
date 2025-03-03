253f3500bbbaf04a227d99474d72948a520d2f6c
HalilOzoglu <HalilOzoglu>
2025-03-03 14:36:42 +0000

    v2.0.1 price column added

--- components/ui/all.jsx
+++ components/ui/all.jsx
@@ -28,25 +28,35 @@ const ProductFamilyTable = () => {
     fetchData();
   }, []);
 
+  const sortDataByPrice = () => {
+    const sortedData = [...data].sort((a, b) => a.price - b.price);
+    setData(sortedData);
+  };
+
   if (loading) return <p>Loading...</p>;
 
   return (
-    <Table aria-label="Product and Family Table">
-      <TableHeader>
-        <TableColumn>Kategori</TableColumn>
-        <TableColumn>Stok Kodu</TableColumn>
-        <TableColumn>Ürün / Aile İsmi</TableColumn>
-      </TableHeader>
-      <TableBody>
-        {data.map((item, index) => (
-          <TableRow key={index}>
-            <TableCell>{item.category}</TableCell>
-            <TableCell>{item.sku}</TableCell>
-            <TableCell>{item.name}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <>
+      <button onClick={sortDataByPrice}>Fiyata Göre Sırala</button>
+      <Table aria-label="Product and Family Table">
+        <TableHeader>
+          <TableColumn>Kategori</TableColumn>
+          <TableColumn>Stok Kodu</TableColumn>
+          <TableColumn>Ürün / Aile İsmi</TableColumn>
+          <TableColumn>Fiyat</TableColumn>
+        </TableHeader>
+        <TableBody>
+          {data.map((item, index) => (
+            <TableRow key={index}>
+              <TableCell>{item.category}</TableCell>
+              <TableCell>{item.sku}</TableCell>
+              <TableCell>{item.name}</TableCell>
+              <TableCell>{item.price}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
   );
 };
 
